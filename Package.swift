f51eae1269fd1eb0beeba9eb3c90da2121fa69d9
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2017-05-25 18:36:23 -0300

    Updating Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "GzipMiddleware",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", Version(2,0,0, prereleaseIdentifiers:["alpha"])),
+        .Package(url: "https://github.com/vapor/vapor.git", Version(2,0,0)),
         .Package(url: "https://github.com/Zewo/zlib.git", majorVersion: 0, minor: 4)
     ]
 )
