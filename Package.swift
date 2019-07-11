a2b71e7f55ef82ed6cdcf0bbda1eff63461d5d8f
Jonathan Thornton <github@jonblatho.com>
2019-07-10 19:50:49 -0500

    try adding a target

--- Package.swift
+++ Package.swift
@@ -6,5 +6,8 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/richardpiazza/CZlib", from: "0.5.0")
+    ],
+    targets: [
+        .target(name: "GzipMiddleware", dependencies: ["Vapor"])
     ]
 )
